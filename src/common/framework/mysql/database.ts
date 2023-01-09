86fac3e2718e23b4ee5c2d783d94ab1fc4d0bd74
Matthew Bell <mbell.dvsa@icloud.com>
2023-01-09 15:07:01 +0000

    Update core deps

--- src/common/framework/mysql/database.ts
+++ src/common/framework/mysql/database.ts
@@ -4,7 +4,7 @@ export const query = async (
   connection: mysql.Connection | mysql.Pool,
   sql: string, args?: any,
 ): Promise<any[]> => {
-  return new Promise((resolve, reject) => {
+  return new Promise<any>((resolve, reject) => {
     connection.query(sql, args, (err, rows) => {
       if (err) {
         reject(err);
